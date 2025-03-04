d32abd06b91c1a505de3b32537f9cbc9d89dfbd6
arshadiyarov <shadiyarovarmat@gmail.com>
2025-03-05 00:29:20 +0500

    fix: site contacts

--- src/shared/constants/contacts.ts
+++ src/shared/constants/contacts.ts
@@ -1,3 +1,3 @@
-export const phoneNumber = "+77758562000";
-export const email = "askaramirbekov5@gmail.com";
-export const instagram = "am1rbekov.a";
+export const phoneNumber = "+77082239268";
+export const email = "shadiyarovarmat@gmail.com";
+export const instagram = "shadiyarov__";
